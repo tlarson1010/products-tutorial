cfa4cf910270ef77e5a364ff73af9e6b788112ca
tlarson1010 <tristynlarson@gmail.com>
2021-09-10 22:35:18 -0600

    Added user object and  product list

--- src/App.js
+++ src/App.js
@@ -2,15 +2,27 @@ import logo from './logo.svg';
 import './App.css';
 import Products from './Products';
 
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
+
+
 function App() {
+
+  const user = {
+    firstName: 'Tristyn',
+    lastName: 'Larson',
+    imageUrl: 'https://picsum.photos/200/300'
+  };
+
   return (
-    <div className="App">
+    <div>
       <h1>
-        Learn React
-        <Products/>
-        <Products/>
-        <Products/>
+        Hello, {formatName(user)}
       </h1>
+      <br/>
+      <img src={user.imageUrl}></img>
+      <Products />
     </div>
   );
 }
