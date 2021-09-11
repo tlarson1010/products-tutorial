1b5af5e8369134f3f3860c93186807ab22088208
tlarson1010 <tristynlarson@gmail.com>
2021-09-11 09:31:04 -0600

    Updated Rating to use useState

--- src/Rating.js
+++ src/Rating.js
@@ -2,18 +2,32 @@ import React from 'react';
 import { IoIosStar, IoIosStarOutline } from 'react-icons/io';
 
 function Rating(props) {
+    /*
+    "useState returns an array with two values: the current state value
+    and a function that lets you update it.
+
+    In the below,  we assign the current state rating value to rating 
+    and the function to update it to setRating.
+
+    We initialize our initial state in useState(props.rating). Our 
+    current state is a single attribute rating which is assigned the 
+    value from props.rating."
+    */
+    const [rating, setRating] = useState(props.rating)
+
+
     return (
         <div>
-            <h1>Rating:{props.rating}</h1>
-            {props.rating >= 1 ? (<IoIosStar/>)
+            <h1>Rating:{rating}</h1>
+            {rating >= 1 ? (<IoIosStar/>)
                 : (<IoIosStarOutline/>)}
-            {props.rating >= 2 ? (<IoIosStar/>)
+            {rating >= 2 ? (<IoIosStar/>)
                 : (<IoIosStarOutline/>)}
-            {props.rating >= 3 ? (<IoIosStar/>)
+            {rating >= 3 ? (<IoIosStar/>)
                 : (<IoIosStarOutline/>)}
-            {props.rating >= 4 ? (<IoIosStar/>)
+            {rating >= 4 ? (<IoIosStar/>)
                 : (<IoIosStarOutline/>)}
-            {props.rating >= 5 ? (<IoIosStar/>)
+            {rating >= 5 ? (<IoIosStar/>)
                 : (<IoIosStarOutline/>)}
         </div>
     );
